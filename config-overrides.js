4be8335a977d4a7340f8789fd023f1390d4831cd
xushanpei <xu_shan_pei@163.com>
2020-02-29 20:18:32 +0800

    移除不必要的包,关闭sourcemap,减少打包体积

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,11 @@
 const { useBabelRc, override, fixBabelImports, addDecoratorsLegacy } = require("customize-cra");
 
+// 补充：对开发友好，打包完成桌面提醒
+// const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+
+// 关闭 sourcemap
+process.env.GENERATE_SOURCEMAP = "false";
+
 module.exports = override(
   useBabelRc(),
   /**启动装饰器 */
